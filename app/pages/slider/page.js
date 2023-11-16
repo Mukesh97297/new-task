9ec95d9e1dca5562f85df0c3feff44da41c0168a
Mukesh <mkverma97297@gmail.com>
2023-11-16 14:44:59 +0530

    first commit

--- app/pages/slider/page.js
+++ app/pages/slider/page.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-function  SlickCarousel() {
-  return (
-    <div>
-      <Carousel>
-        <div>
-            cwnwln
-        </div>
-      </Carousel>
-    </div>
-  )
-}
+// import React from 'react'
+// import Carousel from "react-multi-carousel";
+// import "react-multi-carousel/lib/styles.css";
+// function  SlickCarousel() {
+//   return (
+//     <div>
+//       <Carousel>
+//         <div>
+//             cwnwln
+//         </div>
+//       </Carousel>
+//     </div>
+//   )
+// }
 
-export default  SlickCarousel
+// export default  SlickCarousel
